4599cf5053c029eea6a6aa4e31e08aad51e19972
dew-13 <77990829+dew-13@users.noreply.github.com>
2025-05-06 00:39:33 +0530

    Restructured Directory

--- bookshop/backend/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { checkout } = require('../controllers/orderController');
-const { verifyToken } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-router.post('/checkout', verifyToken, checkout);
-
-module.exports = router;
